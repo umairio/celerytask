fb422e5de8236d1c6c1b15eaacd99bd1da35b886
Muhammad Umair <umairmateen55@gmail.com>
2024-05-08 23:02:43 +0500

    updated workflow

--- django_celery_project/settings.py
+++ django_celery_project/settings.py
@@ -89,15 +89,16 @@ WSGI_APPLICATION = "django_celery_project.wsgi.application"
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.postgresql",
-        "NAME": os.getenv("POSTGRES_DB"),
-        "USER": os.getenv("POSTGRES_USER"),
-        "PASSWORD": os.getenv("POSTGRES_PASSWORD"),
-        "HOST": os.getenv("DB_HOST"),
-        "PORT": os.getenv("DB_PORT"),
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.getenv('DB_NAME', 'appdb'),
+        'USER': os.getenv('DB_USER', 'umair'),
+        'PASSWORD': os.getenv('DB_PASSWORD', 'umair'),
+        'HOST': os.getenv('DB_HOST', 'localhost'),
+        'PORT': os.getenv('DB_PORT', '5432'),
     }
 }
+
 
 
 # Password validation
